b7b64b70a2ee5321d266e49d402f2dff786aa9c8
Noor-Mustafa123 <mustafanoor715@gmail.com>
2024-01-18 13:12:31 +0500

    New Repo practice Project

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//creating object of the pizza class to see output
+        Pizza Pizza1 = new Pizza(true);
+        Pizza1.addToppings(true);
+        Pizza1.addCheese(true);
+        Pizza1.calculatePrice();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
+
 }
--- src/Pizza.java
+++ src/Pizza.java
@@ -2,4 +2,34 @@ public class Pizza {
     private int price ;
     private boolean veg ;
 
+//constructor
+    public Pizza(boolean veg){
+        this.veg = veg ;
+
+        if(this.veg){
+            this.price = 300;
+        }
+        else{
+            this.price = 400;
+        }
+    }
+    //methods
+
+
+   //later on i will add more functionality like types of toppings etc
+    public int addToppings(boolean yes){
+        return this.price += 100;
+    }
+
+    public int addCheese(boolean yes){
+        return this.price += 100;
+    }
+
+    public int addTakeAwayBag(boolean yes){
+        return this.price += 20;
+    }
+
+    public void calculatePrice(){
+        System.out.println(this.price);
+    }
 }
