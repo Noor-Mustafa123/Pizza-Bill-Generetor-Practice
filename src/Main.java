a5bf06188f80687d2aa89d40db45adc98d2aa02c
Noor-Mustafa123 <mustafanoor715@gmail.com>
2024-01-22 14:51:34 +0500

    Refactored the code in the deluxe pizza child class

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,27 @@
 public class Main {
     public static void main(String[] args) {
 //creating object of the pizza class to see output
+        System.out.println("Order 1");
         Pizza Pizza1 = new Pizza(false);
         Pizza1.addaCheese("Mozzarella");
         Pizza1.addaTopping("Peperoni");
         Pizza1.addTheCoupon(5678);
         Pizza1.calculatePrice(4);
 // 2nd object
+        System.out.println("Order 2");
         Pizza Pizza2 = new Pizza(true);
         Pizza2.addaCheese("Cheddar");
         Pizza2.addaTopping("Chicken");
         Pizza2.addTheCoupon(1234);
         Pizza2.calculatePrice(2);
 //deluxe pizza object
+        System.out.println("Order 3");
         deluxePizza dp1 = new deluxePizza(true,"Chicken","Cheddar");
-        dp1.calculatePrice();
+        dp1.addTheCoupon(5678);
+        dp1.calculatePrice(1);
         //2nd try
-        dp1.calculatePrice();
+        System.out.println("Order 4");
+        dp1.calculatePrice(2);
     }
 
 }
--- src/deluxePizza.java
+++ src/deluxePizza.java
@@ -13,42 +13,54 @@ public class deluxePizza extends Pizza {
         else{
             this.deluxePrice = 800;
         }
+//        methods are already called from the beginning
         super.addaTopping(typeOfTopping);
         super.addaCheese(typeOfCheese);
     }
 
 
-
-
-    public void calculatePrice() {
-
-        this.price = this.deluxePrice;
+    @Override
+    public void calculatePrice(int quantity) {
+        this.price = this.deluxePrice * quantity;
 
         //adding the plane pizza price to the total
-        String totalBill = "Deluxe Pizza Price = " + this.deluxePrice + "\n";
+        String totalBill = "Deluxe Pizza Price = " + this.deluxePrice * quantity + "\n";
 
         if(isToppingAdded) {
             int toppingPrice = toppingsType.getOrDefault(typeOfTopping,0);
-            this.price += toppingPrice;
-            totalBill += "Extra "+typeOfTopping+" Toppings Added = " + toppingPrice + "\n";
+            this.price += toppingPrice *quantity;
+            totalBill += "Extra "+typeOfTopping+" Toppings Added = " + toppingPrice * quantity + "\n";
         }
 
 
         if(isCheeseAdded) {
             int cheesePrice = cheeseType.getOrDefault(typeOfCheese,0);
-            this.price += cheesePrice;
-            totalBill += "Extra "+typeOfCheese+" Cheese Added = " + cheesePrice + "\n";
+            this.price += cheesePrice * quantity;
+            totalBill += "Extra "+typeOfCheese+" Cheese Added = " + cheesePrice * quantity+ "\n";
         }
 
         if(isBagAdded) {
-            totalBill += "Bag Price Added = " + this.buyBag + "\n";
-            this.price += this.buyBag;
+            totalBill += "Bag Price Added = " + this.buyBag * quantity + "\n";
+            this.price += this.buyBag * quantity;
+        }
+        int discountPercentage = addTheCoupon(couponNumber);
+        if(quantity>1) {
+            int discountValue = (discountPercentage * this.price) / 100;
+            System.out.println("discount = " + discountPercentage +"%");
+            System.out.println("discount value = " + discountValue);
+            //applying the discount to the total price not per pizza so remove the * quantity
+            this.price -= discountValue ;
+        }
+        else{
+            System.out.println("Discounts are available only for the purchase of more then one pizza!");
         }
 
 
         totalBill +="Total Bill = " + this.price + "\n";
 
         System.out.println(totalBill);
-        }
+    }
+
+
     }
 
